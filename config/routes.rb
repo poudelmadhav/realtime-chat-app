44f887f30d22e75f1132bbc6b0c3db9ed337ce98
Madhav Paudel <poudelmadhav143@gmail.com>
2019-07-29 12:38:05 +0545

    controler actions added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'chats/create'
-  get 'chats/new'
-  get 'chats/show'
-  get 'chats/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :chats
+  root 'chats#index'
 end
